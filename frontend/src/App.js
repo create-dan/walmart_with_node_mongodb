8ca5cc8829554e18d5f137c0fb79de4877fbf662
Dnyaneshwar <dnyaneshwars0611@gmail.com>
2023-04-06 11:26:25 +0530

    walmart-with-node-mongodb-react

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { LoginPage, SignupPage, ActivationPage } from "./Routes";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
+        <Route
+          path="/activation/:activation_token"
+          element={<ActivationPage />}
+        />
+      </Routes>
+      <ToastContainer />
+    </BrowserRouter>
+  );
+};
+
+export default App;
